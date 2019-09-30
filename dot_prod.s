fa077f5d8775ef4dbd9f6a79a796b5ffa35389b8
devrendi <devrendi@asb9804u-c05.csil.sfu.ca>
2019-09-29 21:08:34 -0700

    simplify .s more

--- dot_prod.s
+++ dot_prod.s
@@ -1,28 +1,50 @@
     .globl dot_prod
 dot_prod:
-	# rdi - A
-	# rsi - B
-	# edx - n
-	# ecx - i
-	# r8d - j
 	# C_ij = A_ik * B_jk
-	xorl %eax, %eax
-	push %ebp
-	movl %esp, %ebp
-	push %r12
-	push %r13
-	push %r14
-	movl $0, %14
 
-	jmp loop			# write your code for Question 3 here
+	pushq   %rbp
+	movq    %rsp, %rbp # prologue
 
+	movq    %rdi, -24(%rbp) # rdi - A
+	movq    %rsi, -32(%rbp) # rsi - B
+	movl    %edx, -36(%rbp) # edx - n
+	movl    %ecx, -40(%rbp) # ecx - i
+	movl    %r8d, -44(%rbp) # r8d - j
+	movl    $0, -4(%rbp) # temp return value C_ij
+	movl    $0, -8(%rbp) # loop iterator k
+	jmp     endl
+	
 loop:
-	cmp %r14, edx
-	jg endl
-	mov
-
-
+	
+	movl    -40(%rbp), %eax # eax = i
+	leal    0(,%rax,4), %edx # k = i*4
+	movl    -8(%rbp), %eax
+	addl    %edx, %eax # k = k+i*4
+	movl   	%eax, %edx # edx = k
+	movq    -24(%rbp), %rax # rax = A
+	addq    %rdx, %rax # A += (k+i*4)
+	mov  	(%rax), %eax
+	mov		%eax, %edx
+	movl    -44(%rbp), %eax # eax = j
+	movl  	%eax, %ecx
+	movq    -32(%rbp), %rax # rax = B
+	addq    %rcx, %rax # B += j
+	mov	    (%rax), %eax
+	# movsbl  %al, %eax
+	imull   %edx, %eax # A[k+i*4] * B[j] =  A_ik * B_jk
+	addl    %eax, -4(%rbp) # temp return value += A_ik * B_jk
+	addl    $4, -44(%rbp) # adding +4 to jump to next row on B
+	addl    $1, -8(%rbp) # k++
 
 endl:
+	movl    -8(%rbp), %eax
+	cmpl    -36(%rbp), %eax
+	jl      loop
+	movb   -4(%rbp), %al # put temp return value C_ij inside eax
+	
+	mov 	%rsp, %rbp # epilogue
+	popq    %rbp
 	ret
-
+# movslq S, R is the equivalent of R = SignExtend(S) where R is 64 bits and S is 32 bits.
+# movzbl %al, %edx copy low-byte from register %eax, zero-extend to 4 byte long in %edx
+# movsbl %al, %edx copy low-byte from register %eax, sign-extend to 4 byte long in %edx
--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 char dot_prod(void *A, void *B, int n, int i, int j);
+
+
+char dot_prod_(char *A, char *B, int n, int i, int j)
+{
+  int c = 0;
+  for (int k = 0; k < n; k++)
+    {
+      c += A[k+i*4] * B[j];
+      j+=4;
+    }
+  return c;
+}
+
+
 
 #define N 4
 
@@ -10,29 +25,51 @@ char A[N][N] = { 3,  0,  1,  4,
                  0,  0,  1,  2,
                 -2,  0,  7,  3};
 
-char B[N][N] = { 2, -1,  2, -2,
-                 2, -5, -4,  2,
-                -1, -1,  5,  0,
-                -1,  2, -2, -1};
+// char B[N][N] = { 2, -1,  2, -2,
+//                  2, -5, -4,  2,
+//                 -1, -1,  5,  0,
+//                 -1,  2, -2, -1};
+
 
 
 char C[N][N];
-
+char res[N][N];
+char rand1[N][N];
 void print_matrix(void *A, int n);
 
 
+
+// char* randmatrix(char *A[], int n)
+// {
+// for(int i = 0; i<n; i++)
+//         for(int j = 0; j<n; j++)
+//             A[i][j] = rand() % 256 - 128;
+// return A;
+// }
+
 void main () {
     int i, j;
+    
 
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
             C[i][j] = dot_prod(A, B, N, i, j);
+            res[i][j] =dot_prod_(A, B, N, i, j);
+            if(C[i][j] != res[i][j])
+            {
+                printf("Not equal at i=%d, j=%d",i,j);
+            }
         }
     }
+
+
 
     print_matrix(A, N);
     print_matrix(B, N);
     print_matrix(C, N);
+    print_matrix(res, N);
+
+
     return;
 }
 
